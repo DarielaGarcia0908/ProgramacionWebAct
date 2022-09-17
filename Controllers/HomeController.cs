1ffd9ffee8164613100a875fcd59f97a23134b76
Rodrigo <alejandroguzmancruz@outlook.com>
2022-09-17 02:07:28 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace U1___Actividad_1___ProgramacionWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
